4598039e593d3b0b81f93fe1fcd6def909c1490b
Misha <73934582+misha-ravi@users.noreply.github.com>
2021-10-27 22:53:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Created on Wed Oct 27 22:46:10 2021
+
+@author: MISHA RAVI
+"""
+
+from tkinter import *
+from tkinter import messagebox
+import random
+root=Tk()
+root.title("Billing Managment System")
+root.geometry('1280x720')
+bg_color='#000080'
+
+root.mainloop()
